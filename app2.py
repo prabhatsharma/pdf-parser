38ebe5fe20866d8d67e4bede47d20aad384ddda3
Prabhat Sharma <hi.prabhat@gmail.com>
2017-10-10 22:48:49 -0700

    xml file is bein created now

--- app2.py
+++ app2.py
@@ -9,12 +9,14 @@ file_list = os.listdir('pdfs')
 # print(file_list)
 
 for file in file_list:
-      # print(file)
-      f = file[0:len(file)-4]
-      print(f)
-      txt_command = 'pdf2txt.py -o txt/' + f + '.txt pdfs/' + file
-      print(txt_command)
-      os.system(txt_command)
+    # print(file)
+    f = file[0:len(file)-4]
+    print(f)
+    txt_command = 'pdf2txt.py -o txt/' + f + '.txt pdfs/' + file
+    # print(txt_command)
+    xml_command = 'pdf2txt.py -t xml -o xml/' + f + '.xml pdfs/' + file
+    os.system(txt_command)
+    os.system(xml_command)
 
   # os.system()
 
