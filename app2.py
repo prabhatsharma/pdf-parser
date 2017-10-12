614783ce0f33d6eb65a3036cf8188965df31a808
Prabhat Sharma <hi.prabhat@gmail.com>
2017-10-11 22:03:13 -0700

    now skipping non-pdf files

--- app2.py
+++ app2.py
@@ -10,37 +10,42 @@ file_list = os.listdir('pdfs')
 
 for file in file_list:
     """Iterate over all pdf files"""
-    f = file[0:len(file)-4] # get just the file name withjout extension
-    print(file) # print filename
+    f = file[0:len(file) - 4]  # get just the file name without extension
+    extension = file[len(file)-3:len(file)]
+    print('extension:  ', extension)
+    if extension == 'pdf':
+        print(file)  # print filename
 
-    txt_command = 'pdf2txt.py -o txt/' + f + '.txt pdfs/' + file  # txt extraction command
-    xml_command = 'pdf2txt.py -t xml -o xml/' + f + '.xml pdfs/' + file # xml extraction command
-    os.system(txt_command)  # extract the pdf in txt format and place it in txt folder
-    os.system(xml_command)  # extract the pdf in xml format and place it in xml folder
+        txt_command = 'pdf2txt.py -o txt/' + f + \
+            '.txt pdfs/' + file  # txt extraction command
+        xml_command = 'pdf2txt.py -t xml -o xml/' + f + \
+            '.xml pdfs/' + file  # xml extraction command
+        
+        os.system(txt_command)      # extract the pdf in txt format and place it in txt folder
+        os.system(xml_command)      # extract the pdf in xml format and place it in xml folder
 
-    tree = ET.parse('xml/' + f + '.xml')
-    xml_document = tree.getroot()
-    
-    for text_tag in xml_document.iter('textline'):  # Parse all text
-          word_list = []
-          word = ''
-          font = ''
-          font_size = 10
-          word_location = ''
-          temp_word = {}
-          for character in text_tag:
+        tree = ET.parse('xml/' + f + '.xml')
+        xml_document = tree.getroot()
+
+        for text_tag in xml_document.iter('textline'):  # Parse all text
+            word_list = []
+            word = ''
+            font = ''
+            font_size = 10
+            word_location = ''
+            temp_word = {}
+            for character in text_tag:
                 if character.text != '\n' and character.text != ' ' and character.text != '':
-                      word += character.text
-                      font_size = character.attrib['size']
-                      word_location = character.attrib['bbox']
+                    word += character.text
+                    font_size = character.attrib['size']
+                    word_location = character.attrib['bbox']
                 elif word != '':
-                      temp_word['word'] = word
-                      temp_word['font_size'] = font_size
-                      temp_word['word_location'] = word_location
+                    temp_word['word'] = word
+                    temp_word['font_size'] = font_size
+                    temp_word['word_location'] = word_location
+            if word !='':
+                print(temp_word)
 
-                      print(temp_word)
-
-                      # set word and temp_word to blank so that we can use it to store next word
-                      word = ''
-                      temp_word = {}
-
+            # set word and temp_word to blank so that we can use it to store next word
+            word = ''
+            temp_word = {}
